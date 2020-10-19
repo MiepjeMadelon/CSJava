3e51aef9a265643510d59291152c09d0c09fb9e0
Madelon Bernardy <madelon.bernardy@live.nl>
2020-10-19 09:19:41 +0200

    tge

---
+++ TeGrootException.java
@@ -0,0 +1,16 @@
+/**
+ * Write a description of class TeGrootException here.
+ *
+ * @author Madelon Bernardy
+ * @version 2.0
+ */
+public class TeGrootException extends Exception
+{
+    String invoerSoort; //om bij te houden in welk vak de error is geweest
+    /**
+     * Constructor for objects of class TeGrootException
+     */
+    public TeGrootException(){
+        super();
+    }
+}
